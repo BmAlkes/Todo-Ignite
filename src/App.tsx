f27a220e87a587c1804ef1a97580303367329c84
bmalkes <bmalkes@gmail.com>
2022-08-17 09:04:59 +0300

    feat input

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import { useState } from "react";
 import "./App.css";
+import AddTask from "./components/AddTasks";
+import Header from "./components/Header";
 
 function App() {
-  return <div className="App">ddsf</div>;
+  return (
+    <div className="App">
+      <Header />
+      <div>
+        <AddTask />
+      </div>
+    </div>
+  );
 }
 
 export default App;
